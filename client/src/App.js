9a8ceb99418ce58b350d4c33dd149f9687916171
crypto-jones <integral.being@gmail.com>
2018-10-13 13:00:38 -0700

    Set up React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
 
 class App extends Component {
+  state = {
+    movieList: []
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header" />
+      <div className="App App-header">
+        <Route exact path="/" component={Home} />
       </div>
     );
   }
