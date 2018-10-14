267ff84b9e11b416750c3bc1c5f6c305e0358c93
crypto-jones <integral.being@gmail.com>
2018-10-14 11:55:43 -0700

    Handle unavailable image

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import TextTruncate from 'react-text-truncate';
 
+const noImage =
+  'https://vignette.wikia.nocookie.net/bakemonogatari1645/images/2/26/No-cover-placeholder.png/revision/latest?cb=20171227065818';
+
 const MovieCard = props => {
   const { id, title, overview, poster_path } = props.movie;
   return (
@@ -8,6 +11,10 @@ const MovieCard = props => {
       <img
         src={`https://image.tmdb.org/t/p/w200/${poster_path}`}
         alt="Movie Poster"
+        onError={e => {
+          e.target.onerror = null;
+          e.target.src = noImage;
+        }}
       />
       <div className="movie-text">
         <h2 className="movie-title">{title}</h2>
